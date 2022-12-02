23559d35bec38ce3dfcc26ff139564f9c5bd84e5
wdankier <willemdankier@gmail.com>
2022-12-02 11:09:44 +0100

    support for new domain beheer.socie.nl

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 async function sendTabUrl() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     tab = tabs[0];
-    if (tab && tab.url && tab.url.includes("https://www.socie.nl/beheer")) {
+    if (tab && tab.url && tab.url.includes("https://beheer.socie.nl/")) {
       if (tab.url.includes("members_settings/members")) {
         let msg = {message: 'members_page', url: tab.url};
         chrome.tabs.sendMessage(tab.id, msg, function(response) {console.log(response)});
